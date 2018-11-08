ca58375a94b959ad9369c34cee16ac7c21af28df
lakshyajit165 <lakshyajit165@gmail.com>
2018-11-09 01:03:13 +0530

    Angular CRUD App

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCBpBY8tn4ZW6LJThomAJptiuIK1E6RQhg",
+    authDomain: "angularcrud-e13f6.firebaseapp.com",
+    databaseURL: "https://angularcrud-e13f6.firebaseio.com",
+    projectId: "angularcrud-e13f6",
+    storageBucket: "angularcrud-e13f6.appspot.com",
+    messagingSenderId: "954434752402"
+  },
+  
+  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCBpBY8tn4ZW6LJThomAJptiuIK1E6RQhg",
+    authDomain: "angularcrud-e13f6.firebaseapp.com",
+    databaseURL: "https://angularcrud-e13f6.firebaseio.com",
+    projectId: "angularcrud-e13f6",
+    storageBucket: "angularcrud-e13f6.appspot.com",
+    messagingSenderId: "954434752402"
+  }
 };
 
 /*
